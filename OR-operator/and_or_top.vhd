25da258747f04b2a7fb3e0843b9a790e04726bea
oussama458 <63619120+oussama458@users.noreply.github.com>
2024-10-13 17:32:09 +0100

    Update and_or_top.vhd

--- OR-operator/and_or_top.vhd
+++ OR-operator/and_or_top.vhd
@@ -2,14 +2,14 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
 entity and_or_top is
-    Port ( INA1 : in  STD_LOGIC;    -- AND gate input
-           INA2 : in  STD_LOGIC;    -- AND gate input
-           OA   : out STD_LOGIC );    -- AND gate output
+    Port ( INA1 : in  STD_LOGIC;    -- OR gate input
+           INA2 : in  STD_LOGIC;    -- OR gate input
+           OA   : out STD_LOGIC );    -- OR gate output
 end and_or_top;
 
 architecture Behavioral of and_or_top is
 begin
-    OA <= INA1 OR INA2;    -- 2 input AND gate
+    OA <= INA1 OR INA2;    -- 2 input OR gate
 end Behavioral;
 
 
